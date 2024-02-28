af6a8897314e39309b5bcf9b2d760d59a3e6ddfa
Jeralyn Saenz <saenz.388j@gmail.com>
2024-02-28 03:56:03 -0600

    Added Conjured class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,34 +1,13 @@
 module GildedRose
-
-  def self.new(name, quality, days_remaining)
-    klass_for(name).new(quality, days_remaining)
-  end
-
-  def self.klass_for(name)
-    case name
-    when "Normal Item"
-      Normal
-    when "Aged Brie"
-      Brie
-    when "Sulfuras, Hand of Ragnaros"
-      Item
-    when "Backstage passes to a TAFKAL80ETC concert"
-      Backstage
-    end
-  end
-end
-
   class Item
-    attr_reader :days_remaining, :quality
+    attr_reader :quality, :days_remaining
 
-    def initialize(days_remaining:, quality:)
-      @days_remaining = days_remaining
-      @quality = quality
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
     end
 
     def tick
     end
-
   end
 
 
@@ -45,10 +24,10 @@ end
   class Brie < Item
     def tick
       @days_remaining -= 1
-      return if @quality == 0
+      return if @quality >= 50
 
-      @quality -= 1
-      @quality -= 1 if @days_remaining <= 0
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
     end
   end
 
@@ -63,3 +42,25 @@ end
       @quality += 1 if @days_remaining < 5
     end
   end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item'                               => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake'                        => Conjured }
+
+  def self.new(name:, quality:, days_remaining:)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
+  end
+end
